2aaf394c497b428fa752ffdf86a1af49e3629f2f
Dylan Pugh <73742744+dylanmikai@users.noreply.github.com>
2023-08-21 11:45:49 +0200

    Server setup to serve files in public directory.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const port = 4000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
