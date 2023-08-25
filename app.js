05246384ffe9cda38ac8448e854c8cad53a03b7c
Dylan Pugh <73742744+dylanmikai@users.noreply.github.com>
2023-08-25 10:51:30 +0200

    Work on setting up server to handle form submits.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = 4000;
+const port = 5000;
 
+//Serve files in the public folder to
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+//Parse incoming json data
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const userEmail = req.body.email;
+
+    console.log(userEmail);
+    res.json({ message: 'Email submitted successfully!' });
+})
+
+// Listen on specified port number
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
